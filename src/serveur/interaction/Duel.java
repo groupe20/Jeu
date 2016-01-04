11d1a1b3bc6996a0d44545ac1488f19dde25cc92
21305878 <21305878@u3202-08.ens.fsi>
2016-01-04 15:12:38 +0100

    duel terminal

--- src/serveur/interaction/Duel.java
+++ src/serveur/interaction/Duel.java
@@ -48,6 +48,8 @@ public class Duel extends Interaction<VuePersonnage> {
 				//possibilité de bloquer le coup
 				if(initDefenseur > 50) {
 					perteVie=perteVie/2 ;
+					logs(Level.INFO, Constantes.nomRaccourciClient(defenseur) + " bloque le coup de "
+							+ Constantes.nomRaccourciClient(attaquant));
 				}
 		
 				// degats
