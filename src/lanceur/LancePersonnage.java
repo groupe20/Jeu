0073f0777814194ff92f640565e8e6482f349c92
21305878 <21305878@u3202-01.ens.fsi>
2016-01-04 13:23:10 +0100

    modif n°groupe

--- src/lanceur/LancePersonnage.java
+++ src/lanceur/LancePersonnage.java
@@ -23,7 +23,7 @@ public class LancePersonnage {
 		String nom = "Poutine";
 		
 		// TODO remplacer la ligne suivante par votre numero de groupe
-		String groupe = "G" + Calculs.nombreAleatoire(0,99); 
+		String groupe = "G" + 20; 
 		
 		// nombre de tours pour ce personnage avant d'etre deconnecte 
 		// (30 minutes par defaut)
