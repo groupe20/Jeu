ee1feeddbde11a7e47a1b78a113bf3c11789d8bf
21301545 <21301545@u3202-03.ens.fsi>
2016-01-05 16:46:01 +0100

    lance jeu enzo

--- src/lanceur/LanceJeu.java
+++ src/lanceur/LanceJeu.java
@@ -19,13 +19,18 @@ public class LanceJeu {
 		LancePersonnage.main("Kamikaze", "kami", "1");
 		LancePersonnage.main("Pochtron", "pochtron", "1");
 		LancePersonnage.main("Soigneur", "doc", "1");
+		LancePersonnage.main("Soigneur", "doc", "1");
+		LancePersonnage.main("Soigneur", "doc", "1");
+		LancePersonnage.main("Soigneur", "doc", "1");
+		LancePersonnage.main("Soigneur", "doc", "1");
+		LancePersonnage.main("Soigneur", "doc", "1");
 
 		
-	/*	LancePersonnage.main("Fuyard", "fuyard2", "2");
+		LancePersonnage.main("Fuyard", "fuyard2", "2");
 		LancePersonnage.main("Kamikaze", "kami2", "2");
 		LancePersonnage.main("Pochtron", "pochtron2", "2");
 		LancePersonnage.main("StrategiePersonnage", "strat2", "2");
-*/
+
 
 
 		
