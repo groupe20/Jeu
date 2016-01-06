692439de4d6a6d986803b1770ca5530d564d939a
21305878 <21305878@u3202-09.ens.fsi>
2016-01-06 15:53:48 +0100

    portrait update

--- src/interfacegraphique/interfacesimple/components/Portrait.java
+++ src/interfacegraphique/interfacesimple/components/Portrait.java
@@ -2,8 +2,12 @@ package interfacegraphique.interfacesimple.components;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Polygon;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 /**
@@ -17,44 +21,74 @@ public class Portrait extends JPanel {
 	/**
 	 * Couleur a afficher.
 	 */
-	private Color color;
 	
 	/**
 	 * Vrai si on fait le portrait d'un personnage.
 	 */
-	private boolean personnage;
+	private String personnage;
 	
 	/**
 	 * Cree le portrait d'un element.
 	 * @param c couleur
 	 * @param personnage vrai si on dessine un personnage
 	 */
-	public Portrait(Color c, boolean personnage) {
-		this.color = c;
+	public Portrait( String personnage) {
 		this.personnage = personnage;
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(color);
 		
-		if(personnage) {
-			g.fill3DRect(10, 10, this.getWidth() - 20, this.getWidth() - 20, true);	
-			
-		} else {
-			Polygon p = new Polygon();
-			int coordX, coordY;
-			coordX = getWidth() / 2;
-			coordY = getHeight() / 2;
-			int hauteur = getWidth() - 20;
-			int base = (int) (hauteur * 0.8);
-			
-			p.addPoint(coordX - base/2, coordY + hauteur/2);			
-			p.addPoint(coordX + base/2, coordY + hauteur/2);			
-			p.addPoint(coordX, coordY - hauteur/2);
-			
-			g.fillPolygon(p);
+		Image img=null;
+		
+		switch(this.personnage){
+		
+		case "Pochtron" : try {
+				img = ImageIO.read(new File("images/pochtron(portrait).png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}break;
+		case "Intello" : try {
+				img = ImageIO.read(new File("images/intello(portrait).png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}break;
+		case "Kamikaze" : try {
+				img = ImageIO.read(new File("images/kamikaze(portrait).png"));
+			} catch (IOException e4) {
+				// TODO Auto-generated catch block
+				e4.printStackTrace();
+			}break;
+		case "Soigneur" : try {
+				img = ImageIO.read(new File("images/docteur(portrait).png"));
+			} catch (IOException e3) {
+				// TODO Auto-generated catch block
+				e3.printStackTrace();
+			}break;
+		case "Fuyard" : try {
+				img = ImageIO.read(new File("images/fuyard(portrait).png"));
+			} catch (IOException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			}break;
+		case "other" :	try {
+				img = ImageIO.read(new File("images/potion(portrait).png"));
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}break;
+		case "mortelle" : try {
+				img = ImageIO.read(new File("images/mortelle(portrait).png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}break;
+		
 		}
+		
+	g.drawImage(img, 10 , 10 , null);
 	}
 }
