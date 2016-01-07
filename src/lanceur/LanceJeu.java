2f30cb0cfed5cb08bd529f3210b89b26d9be9e6b
21301545 <21301545@u3202-03.ens.fsi>
2016-01-07 13:48:41 +0100

    maj du jeu enzo

--- src/lanceur/LanceJeu.java
+++ src/lanceur/LanceJeu.java
@@ -21,42 +21,13 @@ public class LanceJeu {
 		LancePersonnage.main("Soigneur", "d", "1");
 		LancePersonnage.main("Crevard", "c", "1");*/
 		LancePersonnage.main("Intello", "i", "1");
-
 		
 
 		LancePersonnage.main("Fuyard", "f3", "3");
-		LancePersonnage.main("Kamikaze", "k3", "3");
-		LancePersonnage.main("Pochtron", "p3", "3");
-		LancePersonnage.main("Soigneur", "d3", "3");
-		LancePersonnage.main("Crevard", "c", "3");
-
-
 
 
 		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
-		LancePotion.main(args);
+
 
 
 	}
